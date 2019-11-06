7de5adfc6c4d9a0251e9d2bd542c064651a081a3
3A432055 <david860120@gmail.com>
2019-11-06 09:51:15 +0800

    4. 設定 routes/web.php 及對應的 Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,7 +9,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('posts', ['as' => 'posts.index',   'uses' => 'PostsController@index']);
+Route::get('post',  ['as' => 'posts.show',    'uses' => 'PostsController@show']);
+Route::get('about',    ['as' => 'posts.about',   'uses' => 'PostsController@about']);
+Route::get('contact',  ['as' => 'posts.contact', 'uses' => 'PostsController@contact']);
